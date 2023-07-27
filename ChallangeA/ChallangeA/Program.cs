3aa9ab6d57023a89fe3869f651411db2881408c0
DESKTOP-DPHMGLJ\Mariusz <marokww@tlen.pl>
2023-07-27 20:15:50 +0200

    Zadanie dzien 4

--- ChallangeA/ChallangeA/Program.cs
+++ ChallangeA/ChallangeA/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//var myAge = 35;
+
+var name = "Ewa";
+char sex = 'k';
+int age = 20;
+
+
+
+if( sex == 'k' )
+{
+    if (age < 30)
+    {
+        Console.Write("Kobieta ponizej 30 lat. ");
+    }
+    else
+    {
+        Console.Write("Kobieta powyzej 30 lat. ");
+    }
+
+    if (name == "Ewa")
+    {
+        Console.WriteLine("O imieniu Ewa, lat " + age);
+    }
+    else
+    {
+        Console.WriteLine("");
+    }
+}
+else
+{
+    if (age < 18)
+    {
+        Console.WriteLine("Niepelnoletni Mezczyzna ");
+    }
+    else
+    {
+        Console.WriteLine("Pelnoletni Meszczyzna");
+    }
+
+}
