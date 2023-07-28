69048d880e4d32ce952e85f7883a44b6d864e6f6
DESKTOP-DPHMGLJ\Mariusz <marokww@tlen.pl>
2023-07-28 10:54:34 +0200

    Dzień 5

--- ChallangeA/ChallangeA/Program.cs
+++ ChallangeA/ChallangeA/Program.cs
@@ -1,40 +1,29 @@
-﻿//var myAge = 35;
+﻿
+/// Wyniki dla liczby: 12353236
+/// 0 -> 0
+/// 1 -> 1
+/// 2 -> 2
+/// 3 -> 3
+/// 4 -> 0
+/// 5 -> 1
+/// 6 -> 1
+/// 7 -> 0
+/// 8 -> 0
+/// 9 -> 0
 
-var name = "Ewa";
-char sex = 'k';
-int age = 20;
 
+long number = new Random().NextInt64(1000000000000,10000000000000);
+string numbersInString = number.ToString();
+char[] letters = numbersInString.ToArray();
+int[] count = new int[10];
 
+for (int i = 0; i < 10; i++)
+    count[i] = 0;
 
-if( sex == 'k' )
-{
-    if (age < 30)
-    {
-        Console.Write("Kobieta ponizej 30 lat. ");
-    }
-    else
-    {
-        Console.Write("Kobieta powyzej 30 lat. ");
-    }
+Console.WriteLine("Wyniki dla liczby:" + numbersInString);
 
-    if (name == "Ewa")
-    {
-        Console.WriteLine("O imieniu Ewa, lat " + age);
-    }
-    else
-    {
-        Console.WriteLine("");
-    }
-}
-else
-{
-    if (age < 18)
-    {
-        Console.WriteLine("Niepelnoletni Mezczyzna ");
-    }
-    else
-    {
-        Console.WriteLine("Pelnoletni Meszczyzna");
-    }
+foreach(char letter in letters)
+    count[letter - '0']++;
 
-}
+for (int i=0; i < 10; i++)
+    Console.WriteLine(i + " -> " + count[i]);
