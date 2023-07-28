dbab8e0e99f4049235442da5a06daa105d2934f0
DESKTOP-DPHMGLJ\Mariusz <marokww@tlen.pl>
2023-07-28 13:31:54 +0200

    Dzień 6

---
+++ ChallangeA/ChallangeA/Employee.cs
@@ -0,0 +1,44 @@
+﻿
+namespace ChallangeA
+{
+    public class Employee
+    {
+        public Employee(string Name = "-", string Surname= "-", uint age = 0)
+        {
+            this.Name = Name;
+            this.Surname = Surname;
+            this.Age = age;
+        }
+
+        public void AddGrade(int grade)
+        {
+            this.grades.Add(grade);
+        }
+        
+        public void PrintGrades()
+        {
+            Console.Write("(");
+            foreach (int grade in grades)
+                Console.Write(grade + ",");
+            Console.WriteLine(")");
+        }
+
+        public string Name { get; }
+
+        public string Surname { get; }
+
+        public uint   Age { get; }
+
+        public int GradesSum
+        {
+            get
+            {
+                return this.grades.Sum();
+            }
+        }
+
+        private List<int> grades = new List<int>();
+
+    }
+
+}
--- ChallangeA/ChallangeA/Program.cs
+++ ChallangeA/ChallangeA/Program.cs
@@ -1,29 +1,30 @@
-﻿
-/// Wyniki dla liczby: 12353236
-/// 0 -> 0
-/// 1 -> 1
-/// 2 -> 2
-/// 3 -> 3
-/// 4 -> 0
-/// 5 -> 1
-/// 6 -> 1
-/// 7 -> 0
-/// 8 -> 0
-/// 9 -> 0
+﻿using ChallangeA;
+
+var rand = new Random();
+List<Employee> employees = new List<Employee>();
+
+employees.Add( new Employee("Adam", "Kowalski", 32) );
+employees.Add(new Employee("Monika", "Nowak", 19));
+employees.Add(new Employee("Zuzia", "Bąk", 44));
+
+foreach( Employee e in employees )
+    for(int i=0; i < 5; i++)
+    {
+        e.AddGrade( rand.Next(1,10+1) );
+    }
+
+int biggestScore = 0;
+
+foreach (Employee e in employees)
+    if (e.GradesSum > biggestScore)
+        biggestScore = e.GradesSum;
 
 
-long number = new Random().NextInt64(1000000000000,10000000000000);
-string numbersInString = number.ToString();
-char[] letters = numbersInString.ToArray();
-int[] count = new int[10];
+foreach (Employee e in employees)
+    if (e.GradesSum == biggestScore)
+    {
+        Console.WriteLine("Pracownik: " + e.Name + " " + e.Surname + " w wieku " + e.Age);
+        Console.WriteLine("Ma najwiekszą liczbę ocen = " + e.GradesSum );
+        e.PrintGrades();
+    }
 
-for (int i = 0; i < 10; i++)
-    count[i] = 0;
-
-Console.WriteLine("Wyniki dla liczby:" + numbersInString);
-
-foreach(char letter in letters)
-    count[letter - '0']++;
-
-for (int i=0; i < 10; i++)
-    Console.WriteLine(i + " -> " + count[i]);
